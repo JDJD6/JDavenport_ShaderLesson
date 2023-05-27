81cb1fa9232fbb7269e6160c413ebb41b6580a69
JDJD6 <89411423+JDJD6@users.noreply.github.com>
2023-05-26 19:49:56 -0700

    111111111111111111111111111111111111111111111111

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class SpawnManager : NetworkBehaviour
+{
+    public GameObject[] lilyPadObjs = null;
+    // Start is called before the first frame update
+    public override void OnStartServer()
+    {
+        InvokeRepeating("SpawnLilyPad", 2.0f, 5.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SpawnLilyPad()
+    {
+        foreach(GameObject lilyPad in lilyPadObjs)
+        {
+            GameObject tempLilyPad = Instantiate(lilyPad);
+            Mirror.NetworkServer.Spawn(tempLilyPad);
+        }
+       
+    }
+}
